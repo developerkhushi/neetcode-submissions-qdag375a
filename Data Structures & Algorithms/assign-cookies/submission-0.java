653262c2bd2f0e458b7546eb4330b99cc8b8a6a7
Khushboo Singh <64570880+developerkhushi@users.noreply.github.com>
2026-04-02 15:39:27 +0530

    Add: assign-cookies - submission-0

---
+++ Data Structures & Algorithms/assign-cookies/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int left = 0, right = 0;
+        while (left < g.length && right < s.length) {
+            if (g[left] <= s[right]) {
+                left++;
+            }
+            right++;
+        }
+        return left;
+    }
+}
